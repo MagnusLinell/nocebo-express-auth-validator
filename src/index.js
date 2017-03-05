d5ff0351207fd924342444ccf73c20bd45a33a6a
Magnus Linell <magnus.linell@gmail.com>
2017-03-05 20:41:00 +0100

    Added authorization token into auth in express

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ const validateOptions = (options) => {
 };
 
 const validateAuthorization = (options) => {
+  return (req, res, next) => {
+    req.auth = req.auth || {};
+    req.auth.bearer = req.headers.authorization && req.headers.authorization.split(' ').length > 1 ? req.headers.authorization.split(' ')[1] : '';
     return expressJwt({
       'secret': new Buffer(options.jwtSecret, 'base64')
-    });
+    })(req, res, next);
+  };
 };
 
 const validateUser = (req, res, next) => {
